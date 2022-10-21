e123bee9663fb893958f035b73cfce59e5fa4b3b
jadshaker <jaadshaker@gmail.com>
2022-10-21 16:54:24 +0300

    update

--- codeforces/1744C Traffic Light.cpp
+++ codeforces/1744C Traffic Light.cpp
@@ -5,12 +5,24 @@ typedef long long ll;
 
 int solve()
 {
-    int n;
+    int n, count, res = 0;
     char c;
-    string s;
-    cin >> n >> c >> s;
+    cin >> n >> c;
+    char s[n * 2];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i];
+        s[n + i] = s[i];
+    }
     if (c == 'g') return 0;
-    
+    for (int i = 0; i < n; i++)
+        if (s[i] == c)
+        {
+            count = 0;
+            while (s[i] != 'g') count++, i++;
+            res = max(count, res);
+        }
+    return res;
 }
 
 int main()
