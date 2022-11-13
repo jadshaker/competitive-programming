eeaf6de2241edf7e5be0a030c5e17941b423fd1f
jadshaker <jaadshaker@gmail.com>
2022-11-13 15:21:27 +0200

    update

---
+++ codeforces/1748A The Ultimate Square.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << (n + 1) / 2 << '\n';
+    }
+}
